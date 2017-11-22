0d1f064811dc0ebf65163d84a2f16168e374c1a1
psimoes <p.simoes@campus.fct.unl.pt>
2017-11-22 18:35:47 +0000

    updated bringup script

--- bringup.sh
+++ bringup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-sudo /sbin/ip link set can0 up type can bitrate 500000
-sudo /sbin/ip link set can0 up type can bitrate 500000
-
+sudo killall java
 sudo java -Djava.library.path=/usr/lib/rxtx:/usr/lib/jni -jar /home/pi/C2NET/nodec2net/target/IoT_node-0.1.1-SNAPSHOT-jar-with-dependencies.jar
